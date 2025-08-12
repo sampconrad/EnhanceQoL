2dc02495b1f3a6c06afaed76c8290181c263a30d
R41z0r <philipp.barth@cloudra.de>
2025-08-12 05:34:25 +0200

    update locale

--- EnhanceQoLCombatMeter/Locales/deDE.lua
+++ EnhanceQoLCombatMeter/Locales/deDE.lua
@@ -1,21 +1,2 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_CombatMeter", "deDE")
 if not L then return end
-
-L["Combat Meter"] = "Kampf-Meter"
-L["Enabled"] = "Aktiviert"
-L["Always Show"] = "Immer anzeigen"
-L["Always Show Self"] = "Sich selbst immer anzeigen"
-L["Update Rate"] = "Aktualisierungsrate"
-L["Font Size"] = "Schriftgröße"
-L["Name Length"] = "Namenslänge"
-L["Reset"] = "Zurücksetzen"
-L["Groups"] = "Gruppen"
-L["Add Group"] = "Gruppe hinzufügen"
-L["Remove"] = "Entfernen"
-L["Bar Width"] = "Balkenbreite"
-L["Bar Height"] = "Balkenhöhe"
-L["Max Bars"] = "Max. Balken"
-L["DPS"] = "DPS"
-L["Damage Overall"] = "Schaden (Gesamt)"
-L["Healing Per Fight"] = "Heilung (Kampf)"
-L["Healing Overall"] = "Heilung (Gesamt)"
